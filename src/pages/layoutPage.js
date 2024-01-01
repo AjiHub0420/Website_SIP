94eb81dfd4e61bc9f88fcd96502236b8973a3a3a
TanNicholas80 <tannicholas54@gmail.com>
2024-01-02 05:16:11 +0700

    Penambahan Map dan daerah yang padam listriknya

--- src/pages/layoutPage.js
+++ src/pages/layoutPage.js
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NavLayout from "../components/navbarComponent";
-import Container from 'react-bootstrap/Container'
+
 const BaseLayout = () => {
     return (<>
         <NavLayout />
-        <Container>
-            <Outlet />
-        </Container>
+        <Outlet />
     </>)
 };
 
